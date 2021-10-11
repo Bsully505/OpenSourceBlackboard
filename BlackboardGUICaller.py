a6aac58ea882e137ff63109f677d192d513262d8
Bsully505 <Bsully505@yahoo.com>
2021-10-11 12:24:24 -0400

    added new functions that call the api with a header contianing a cookie from a session

--- BlackboardGUICaller.py
+++ BlackboardGUICaller.py
@@ -4,7 +4,7 @@ import requests
 #import LoginPage
 from LoginPage import LoginPage
 from LoggedInAPITester import LoggedInAPITester
-
+import json
 
 
 
@@ -38,6 +38,7 @@ class mainApp(tk.Tk):
             self.ChangeFrame("LoginPage")
 
         def ValidateLogin(self,Username, Password):
+            self.GetCoursesTaken();
             print("Validates Login")
             username = Username
             password = Password
@@ -58,14 +59,28 @@ class mainApp(tk.Tk):
 
         def inportHeader(self):
             #example course id is _90767_1 this is for procedural generation
-            input = inputText.get(1.0, "end-1c")
-            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/"+input+"/contents"
-            response = requests.request("GET",url)
+
+            headers = {
+                'Cookie': 'JSESSIONID=7025945EA3C30F0F32950F244F8BB5DC; BbClientCalenderTimeZone=America/New_York; web_client_cache_guid=293fa897-374c-4184-8348-aaea3369328a; COOKIE_CONSENT_ACCEPTED=true; OptanonConsent=isIABGlobal=false&datestamp=Wed+Oct+06+2021+17:41:07+GMT-0400+(Eastern+Daylight+Time)&version=6.19.0&hosts=&consentId=d95e8315-f74c-4afb-b76b-df93befcae93&interactionCount=1&landingPath=https://www.blackboard.com/&groups=C0001:1,C0003:1,BG1:1,C0002:1,C0005:1,C0004:1; JSESSIONID=511025438F8E18B50C366B963BC29E47; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; BbRouter=expires:1633977941,id:B92E7FAEE20055E9F718395E924C8EC0,signature:3c4665e3d90db419a4ffea3ba4722877a9534daa89f6fa1d122e9f7916543900,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:afdf8090-3457-4e34-aed0-6c92002803df'
+                }
+
+            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/"+"_90767_1"+"/contents"
+            response = requests.request("GET",url,headers = headers)
 
             print(response.json())
 
 
+        def GetCoursesTaken(self):
+            headers = {
+                'Cookie': 'JSESSIONID=7025945EA3C30F0F32950F244F8BB5DC; BbClientCalenderTimeZone=America/New_York; web_client_cache_guid=293fa897-374c-4184-8348-aaea3369328a; COOKIE_CONSENT_ACCEPTED=true; OptanonConsent=isIABGlobal=false&datestamp=Wed+Oct+06+2021+17:41:07+GMT-0400+(Eastern+Daylight+Time)&version=6.19.0&hosts=&consentId=d95e8315-f74c-4afb-b76b-df93befcae93&interactionCount=1&landingPath=https://www.blackboard.com/&groups=C0001:1,C0003:1,BG1:1,C0002:1,C0005:1,C0004:1; JSESSIONID=511025438F8E18B50C366B963BC29E47; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; BbRouter=expires:1633977941,id:B92E7FAEE20055E9F718395E924C8EC0,signature:3c4665e3d90db419a4ffea3ba4722877a9534daa89f6fa1d122e9f7916543900,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:afdf8090-3457-4e34-aed0-6c92002803df'
+                }
+            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/"
+            response = requests.request("GET",url,headers = headers)
+            jsonText = response.json()
+            for attr in jsonText:
 
+
+            #rint(json.dumps(jsonText,indent = 4, sort_keys=True))
 
 
         def printResponseForCourseInfo(self):
--- LoggedInAPITester.py
+++ LoggedInAPITester.py
@@ -8,24 +8,7 @@ class LoggedInAPITester(tk.Frame):
 
         #row is up and down an column is the left or right
         #origin is top left
-
-        labelUser = ttk.Label(self, text = "UserName: ")
-
-        textUser = tk.Text(self,height=1 ,width = 15)
-
-        labelPass = ttk.Label(self, text = "Password: ")
-
-        textPass = tk.Text(self,height = 1,width = 15)
-
-        Submit = tk.Button(self,text = "Submit", command = lambda : controller.ChangeFrame("LoginPage"))
-        Submit.grid(row=6, column =4)
-        labelUser.grid(row = 2, column = 2)
-        textUser.grid(row = 2,column = 4)
-        labelPass.grid(row = 4, column = 2)
-        textPass.grid(row = 4, column = 4)
-
-        #label "Username:"    #text box for user
-        #label "Password:"    text box for password
+        
 
         #Button for submitting login
         #button command tests if the username and password is good and then changes fram to loggedinPage
