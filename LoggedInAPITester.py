a65efd833e894ff87ed5aa8622da8818eb1312c0
Bsully505 <Bsully505@yahoo.com>
2021-10-12 14:21:43 -0400

    before

--- LoggedInAPITester.py
+++ LoggedInAPITester.py
@@ -6,13 +6,7 @@ class LoggedInAPITester(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
-        #row is up and down an column is the left or right
-        #origin is top left
+        #ideas is to be able be logged in and call an api using buttons 
+
         
 
-        #Button for submitting login
-        #button command tests if the username and password is good and then changes fram to loggedinPage
-    def toString():
-        return "LoggedInAPITester"
-def printText(self):
-    input = textUser
