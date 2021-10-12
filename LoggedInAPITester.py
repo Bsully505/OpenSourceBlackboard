31c29abde61ead1b322df2fbcab6c5331ee449d2
Colin M <colintmcneill@gmail.com>
2021-10-12 14:46:52 -0400

    added non-functional buttons to LoggedInAPITester

--- LoggedInAPITester.py
+++ LoggedInAPITester.py
@@ -5,7 +5,25 @@ from tkinter import ttk
 class LoggedInAPITester(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
+        controller.geometry('250x450')
+        labelCourses = ttk.Label(self, text = "Get list of courses")
+        buttonCourses = tk.Button(self, text = "Courses", command = lambda:controller.GetCoursesTaken())
+        labelGrades = ttk.Label(self, text = "Get list of grades")
+        buttonGrades = tk.Button(self, text = "Grades", command = lambda:controller.GetCoursesTaken())
+        labelAnnouncements = ttk.Label(self, text = "Get list of announcements")
+        buttonAnnouncements = tk.Button(self, text = "Announcements", command = lambda:controller.GetCoursesTaken())
+        labelUpcoming = ttk.Label(self, text = "Get list of upcoming assignments")
+        buttonUpcoming = tk.Button(self, text = "Upcoming Assignments", command = lambda:controller.GetCoursesTaken())
+        labelCourses.pack()
+        buttonCourses.pack()
+        labelGrades.pack()
+        buttonGrades.pack()
+        labelAnnouncements.pack()
+        buttonAnnouncements.pack()
+        labelUpcoming.pack()
+        buttonUpcoming.pack()
 
+    
         #ideas is to be able be logged in and call an api using buttons 
 
         
