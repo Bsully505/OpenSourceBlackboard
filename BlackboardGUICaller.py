9d21d45a36e0104c31e6f7b84e3a7fbe2d32de49
Bsully505 <Bsully505@yahoo.com>
2021-10-14 21:51:05 -0400

    changed the way cources are got

--- BlackboardGUICaller.py
+++ BlackboardGUICaller.py
@@ -12,7 +12,7 @@ class mainApp(tk.Tk):
         global username
         global password
         header = {
-        'Cookie': 'JSESSIONID=1D649AA6BF0918ABB0CEC762AE99907F; BbClientCalenderTimeZone=America/New_York; web_client_cache_guid=293fa897-374c-4184-8348-aaea3369328a; COOKIE_CONSENT_ACCEPTED=true; OptanonConsent=isIABGlobal=false&datestamp=Wed+Oct+06+2021+17:41:07+GMT-0400+(Eastern+Daylight+Time)&version=6.19.0&hosts=&consentId=d95e8315-f74c-4afb-b76b-df93befcae93&interactionCount=1&landingPath=https://www.blackboard.com/&groups=C0001:1,C0003:1,BG1:1,C0002:1,C0005:1,C0004:1; JSESSIONID=C2ED5B2938F1AC008B73AE97954D2DF9; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED9920FD0104AE518E1D40FACC426803F95C7A6B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED9920FD0104AE518E1D40FACC426803F95C7A6B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; BbRouter=expires:1634003558,id:B92E7FAEE20055E9F718395E924C8EC0,signature:66c9d74c699da4f1a9466591f7d2f992102a8a7161edc1a4242beab34ee72832,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:afdf8090-3457-4e34-aed0-6c92002803df',
+        'Cookie': 'JSESSIONID=93F302898351D258EED37AD3B415DDC8; BbClientCalenderTimeZone=America/New_York; JSESSIONID=48E5B689F468CDE38039E367D03A7C84; web_client_cache_guid=71337fe4-925c-420e-8169-1a508df27f16; COOKIE_CONSENT_ACCEPTED=true; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; BbRouter=expires:1634266155,id:77801EE8716510CC45DDE3E67E42D7A1,signature:ff2c32b7290c5a17acc09ae4751d4e83c5c5a835449d08e74b4b89c338666811,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:10d4a307-1620-4144-8d10-b98b8f4c5e02',
         'courseId': '_90767_1'
         }
 
@@ -37,7 +37,7 @@ class mainApp(tk.Tk):
                 self.frames[page_name] = frame
                 frame.grid(row = 0, column = 0, sticky = "nsew")
 
-
+            self.GetCoursesTaken();
             self.ChangeFrame("LoginPage")
 
         def ValidateLogin(self,Username, Password):
@@ -73,15 +73,22 @@ class mainApp(tk.Tk):
 
         def GetCoursesTaken(self):
             headers = self.header
-            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/?offset=1000"
+            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/?offset=21000"
             response = requests.request("GET",url,headers = headers)
             jsonText = response.json()
-            #for open in jsonText['results']:
-            #    for attr in open:
-            #        print(attr)
-            #        print(attr.index)
-
-            print(json.dumps(jsonText,indent = 4, sort_keys=True))
+            nextUrl =jsonText['paging']['nextPage']
+            while('paging' in jsonText):
+                print(len(jsonText))
+                
+                
+                print(nextUrl.split("=")[1])
+                nextUrl =jsonText['paging']['nextPage']
+                url = "https://quinnipiac.blackboard.com"+ nextUrl
+                response = requests.request("GET",url,headers = headers)
+                jsonText = response.json()
+                
+                #for loop goes here 
+                
 
 
         def printResponseForCourseInfo(self):
