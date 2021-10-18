fd9c3f1b5f2c1671a1484aaf2a1b623e34e0d7d2
Bsully505 <Bsully505@yahoo.com>
2021-10-18 14:19:04 -0400

    found better way to get courses

--- BlackboardGUICaller.py
+++ BlackboardGUICaller.py
@@ -7,12 +7,18 @@ from LoggedInAPITester import LoggedInAPITester
 import json
 
 
+#the question that I(Bryan) have is how to get the courses faster
+#the answer is to use a different api call which is in the course meembership folder
 
+#first thing to do to get courses 
+#get the username from user 
+#do search https://quinnipiac.blackboard.com/learn/api/public/v1/users?userName=bcsullivan
 class mainApp(tk.Tk):
         global username
         global password
+        global userID
         header = {
-        'Cookie': 'JSESSIONID=93F302898351D258EED37AD3B415DDC8; BbClientCalenderTimeZone=America/New_York; JSESSIONID=48E5B689F468CDE38039E367D03A7C84; web_client_cache_guid=71337fe4-925c-420e-8169-1a508df27f16; COOKIE_CONSENT_ACCEPTED=true; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; BbRouter=expires:1634266155,id:77801EE8716510CC45DDE3E67E42D7A1,signature:ff2c32b7290c5a17acc09ae4751d4e83c5c5a835449d08e74b4b89c338666811,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:10d4a307-1620-4144-8d10-b98b8f4c5e02',
+        'Cookie': 'JSESSIONID=4A5A37413A9F38C27813A90957B1A078; BbClientCalenderTimeZone=America/New_York; web_client_cache_guid=71337fe4-925c-420e-8169-1a508df27f16; COOKIE_CONSENT_ACCEPTED=true; JSESSIONID=795CC5E2FD4C08B446E4C94298548E90; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED9920FD0104AE518E1D40FACC426803F95C7A3C12E3FB3F56CA8A62BEA365FD369A677D7A1DDDCFCE04BDBD33143BAEBB05A1; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED9920FD0104AE518E1D40FACC426803F95C7A3C12E3FB3F56CA8A62BEA365FD369A677D7A1DDDCFCE04BDBD33143BAEBB05A1; BbRouter=expires:1634502482,id:DE92A866002A8580F93D3F6A9CD7CF74,signature:88556a6b9d78321459c5f1b7c9a10d68db003f5fbe941fff8c2c42aee1048bca,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:9f02d456-1c12-4fda-ae4c-34212336c88e',
         'courseId': '_90767_1'
         }
 
@@ -36,15 +42,17 @@ class mainApp(tk.Tk):
                 frame = F(container, self)
                 self.frames[page_name] = frame
                 frame.grid(row = 0, column = 0, sticky = "nsew")
-
-            self.GetCoursesTaken();
+            self.ValidateLogin('bcsullivan', 'FakePassword123')
+            #self.GetCoursesTaken();
             self.ChangeFrame("LoginPage")
 
         def ValidateLogin(self,Username, Password):
-            self.GetCoursesTaken();
-            print("Validates Login")
+            
             username = Username
+            username = 'bcsullivan'
             password = Password
+            userID= self.getUserId(username)
+            self.GetCoursesTaken();
             self.ChangeFrame("LoggedInAPITester")
 
 
@@ -73,23 +81,35 @@ class mainApp(tk.Tk):
 
         def GetCoursesTaken(self):
             headers = self.header
-            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/?offset=21000"
+            
+            url = "https://quinnipiac.blackboard.com/learn/api/public/v1/courses/?offset=100&courseId=_90503_1"
             response = requests.request("GET",url,headers = headers)
             jsonText = response.json()
-            nextUrl =jsonText['paging']['nextPage']
-            while('paging' in jsonText):
-                print(len(jsonText))
-                
-                
-                print(nextUrl.split("=")[1])
+            
+            #only one page 
+            if('paging' not in jsonText):
+                print(jsonText);
+                print("First and only")
+            else:
+
                 nextUrl =jsonText['paging']['nextPage']
-                url = "https://quinnipiac.blackboard.com"+ nextUrl
-                response = requests.request("GET",url,headers = headers)
-                jsonText = response.json()
+
+            #loops through all of the api results and stops at last page
+            
+                while('paging' in jsonText):
+                    print(nextUrl.split("=")[1])
+                    #print(jsonText)
+                    nextUrl =jsonText['paging']['nextPage']
+                    url = "https://quinnipiac.blackboard.com"+ nextUrl#"&courseid=_90503_1"
+                    response = requests.request("GET",url,headers = headers)
+                    jsonText = response.json()
                 
                 #for loop goes here 
                 
-
+        def getUserId(username):
+            url = 'https://quinnipiac.blackboard.com/learn/api/public/v1/users?userName='+username
+            response = requests.request("GET",url)
+            return response.json()['results']['id']
 
         def printResponseForCourseInfo(self):
             #example course id is _90767_1 this is for procedural generation
