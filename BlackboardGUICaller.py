cbf6b9334f5d29791b4bb701862cf1f78347a40a
Colin M <colintmcneill@gmail.com>
2021-10-19 13:54:01 -0400

    prints unformatted announcements to console

--- BlackboardGUICaller.py
+++ BlackboardGUICaller.py
@@ -30,7 +30,7 @@ class mainApp(tk.Tk):
         courses = []
         preUrl = 'https://quinnipiac.blackboard.com/'
         header = {
-        'Cookie': 'JSESSIONID=B9A00112811A9CD22B1A8F7F05A4A09F; BbClientCalenderTimeZone=America/New_York; web_client_cache_guid=2f5ecba6-3258-4b3d-977a-ecae91e35061; COOKIE_CONSENT_ACCEPTED=true; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED99208A7AE0BA845CCE05C9AA0AF618C176F06B32BF6DD3D862C1CD713BC0C9D816EACFBD71EF3251F933A12E1A0B164E8378; JSESSIONID=45DAC7F1DEFC0228588BF6F9F3214201; BbRouter=expires:1634604019,id:B3D688374C3E9EA55D7FD765D4345C40,signature:9bb512f069408b00076d4c915d27ba5bf06c16444f853d613f5c9314c9839ad4,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:cff58183d5a742ad9d5c0d751bc05a16,v:2,xsrf:3eb7d13a-1c7a-4897-9e5b-b16d4d16b7e1'
+        'Cookie': 'JSESSIONID=3861BEAEB4D7A84336F6037C1B18FA36; COOKIE_CONSENT_ACCEPTED=true; BbClientCalenderTimeZone=America/New_York; web_client_cache_guid=2e2c6e9c-1a2b-4870-887c-f3f8fd8fff9b; JSESSIONID=863D1219E798EB42FFEFFF9447579314; AWSELB=6FBB59590AD55E4479C8D5228AC967BB0DA2ED9920FD0104AE518E1D40FACC426803F95C7A3C12E3FB3F56CA8A62BEA365FD369A677D7A1DDDCFCE04BDBD33143BAEBB05A1; AWSELBCORS=6FBB59590AD55E4479C8D5228AC967BB0DA2ED9920FD0104AE518E1D40FACC426803F95C7A3C12E3FB3F56CA8A62BEA365FD369A677D7A1DDDCFCE04BDBD33143BAEBB05A1; BbRouter=expires:1634675527,id:E3150EE5084E68240A219EE32C5D627E,signature:316fb97ce46fe6ae0bf118f6e6fd0b81ab4f79251fedb3b265381d31d4c5283e,site:2bf58a57-0609-4ded-b883-0cffff2406fd,timeout:10800,user:6ce6db0ef8e24509a7c1abf1e60b3845,v:2,xsrf:f71cdf56-21bc-431e-816a-cfe4c6186591'
         }
 
 
@@ -54,7 +54,7 @@ class mainApp(tk.Tk):
                 frame.grid(row = 0, column = 0, sticky = "nsew")
 
             #self.ValidateLogin('cjflannery', 'FakePassword123') this was to test if when using the token from bcsullivan can i access someone elses account does not work
-            self.ValidateLogin('bcsullivan', 'FakePassword123')
+            self.ValidateLogin('ctmcneill', 'FakePassword123')
             self.ChangeFrame("LoginPage")
 
         def ValidateLogin(self,Username, Password):
@@ -65,6 +65,7 @@ class mainApp(tk.Tk):
             self.printResponseForCourseInfo()
             self.printoutCourseIDs()
             self.ChangeFrame("LoggedInAPITester")
+            self.printoutAnnouncements()
 
 
 
@@ -79,7 +80,7 @@ class mainApp(tk.Tk):
 
 
         #not a function that is being used
-        def inportHeader(self):
+        def importHeader(self):
             #example course id is _90767_1 this is for procedural generation
             #this would possibly be where selenium is used to capture the cookie of jsessionid
             print('get session cookie and set to header')
@@ -110,6 +111,16 @@ class mainApp(tk.Tk):
         def printoutCourseIDs(self):
             for i in self.courses:
                 print(i)
+
+        def printoutAnnouncements(self):
+            url = self.preUrl + '/learn/api/public/v1/courses/_90503_1/announcements'
+            response = requests.request("GET", url, headers = self.header)
+            #print(response.json()['name'] + " it worked!")
+            for res in response.json()['results']:
+                #print(res)
+                print(res['title'])
+                print(res['body'])
+
 
 
 if __name__ == "__main__":
